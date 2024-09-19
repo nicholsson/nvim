b00d7c0706fb390ba18d5df1bf4455bd6e964516
nicholsson <nikkg27@gmail.com>
2024-09-19 19:23:57 +0200

    set colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -13,22 +13,6 @@
 }--]]
 --[[return {
 	{
-		"projekt0n/github-nvim-theme",
-		lazy = false, -- make sure we load this during startup if it is your main colorscheme
-		priority = 1000, -- make sure to load this before all the other start plugins
-		config = function()
-			require("github-theme").setup({
-				options = {
-					transparent = true,
-				},
-			})
-
-			vim.cmd("colorscheme github_dark")
-		end,
-	},
-}--]]
-return {
-	{
 		"navarasu/onedark.nvim",
 		lazy = false,
 		priority = 1000,
@@ -40,32 +24,18 @@ return {
 			vim.cmd.colorscheme("onedark")
 		end,
 	},
+}--]]
+return {
+	{
+		"sainnhe/everforest",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.o.background = "light"
+			vim.cmd.colorscheme("everforest")
+		end,
+	},
 }
---[[return {
-	{
-		"sainnhe/gruvbox-material",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			vim.g.gruvbox_material_transparent_background = 2
-			--	vim.g.gruvbox_material_background = "hard"
-			vim.cmd.colorscheme("gruvbox-material")
-		end,
-	},
-}--]]
---[[return {
-	{
-		"ayu-theme/ayu-vim",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			vim.opt.termguicolors = true
-			vim.g.ayucolor = "dark"
-			-- dark, mirage, light
-			vim.cmd.colorscheme("ayu")
-		end,
-	},
-}--]]
 --[[return {
 	{
 		"rose-pine/neovim",
@@ -78,7 +48,7 @@ return {
 					transparency = true,
 				},
 			})
-			vim.cmd.colorscheme("rose-pine")
+			vim.cmd.colorscheme("rose-pine-dawn")
 			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 		end,
